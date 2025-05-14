4263fdbea03e1a5113da7c0e803ebe2235d0e6c5
sanie <sanie07@mail.ru>
2025-05-14 19:39:28 +0000

    Commit4

---
+++ app_v2.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import time
+
+app = Flask(__name__)
+
+request_count = 0
+
+@app.route("/time")
+def get_time():
+    global request_count
+    request_count += 1
+    return jsonify({"time": int(time.time())})
+
+@app.route('/metrics')
+def get_metrics():
+    return jsonify({"count": request_count})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3031)
