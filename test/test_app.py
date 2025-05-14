2bca30886c5c13f74651a9cf0cd6ac780f661677
sanie <sanie07@mail.ru>
2025-05-14 11:47:27 +0000

    Изменены тесты

--- test/test_app.py
+++ test/test_app.py
@@ -1,14 +1,17 @@
+import sys
+import os
 import pytest
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app
 
 @pytest.fixture
 def client():
+    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-def test_time_route(client):
+def test_time_route_not_zero(client):
     response = client.get('/time')
     assert response.status_code == 200
     data = response.get_json()
-    assert 'time' in data
-    assert data['time'] != 0
+    assert data["time"] != 0
